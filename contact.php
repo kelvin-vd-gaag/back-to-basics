403bfc1dae7b02507f7ebd13c97fbdcd88bbe61c
Kelvin van der Gaag <kelvinvandergaag@protonmail.com>
2023-11-21 13:29:56 +0100

    Opdracht 1

---
+++ contact.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php require "navigatie.php" ?>
+    <a href="tel:06-12312321">Bel me nu</a>
+</body>
+</html>
---
+++ navigatie.php
@@ -0,0 +1,20 @@
+<nav>
+        <span>Vandaag is het  <?php echo date("d") . "  " . date("m"); ?> </span>
+        <ul>
+            <li><a href="index.html">Thuis</a></li>
+            <li><a href="contact.html">Contact</a></li>
+            <li><a href="contact.html">link</a></li>
+            <li><a href="contact.html">link</a></li>
+            <li><a href="contact.html">link</a></li>
+            <li><a href="contact.html">link</a></li>
+            <li><a href="contact.html">link</a></li>
+        </ul>
+    </nav>
+
+
+    <?php
+    $maanden  = ["Januari", "Februari",
+     "Maart", "April", "Mei","Juni","Juli","Augustus","September","Oktober","November","December"];
+
+     echo $maanden[0];
+    ?>
